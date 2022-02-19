3be3128b9014c12dd5b633089a2b52a8d5db1bc3
Dominik Raffelsberg <raffelsberg.d19@htlwienwest.at>
2022-02-19 16:22:50 +0100

    Memory - The Game finished

--- memory/js/code.js
+++ memory/js/code.js
@@ -1,6 +1,16 @@
 let openCards = [];
 let numberCards = 16;
 let count = 0;
+let list = []
+let picture = ["../img/baljeet.png", "../img/buford.png", "../img/doofenschmirtz.png", "../img/ferb.png", "../img/isabella.png", "../img/perry.png", "../img/phineas.png", "../img/Candice.png"]
+let found = 0
+
+let firstClick = 0
+let countTime = 0
+let timeractive = false
+let playInterval
+let highscore = 0
+let score = 0
 
 createDeck(numberCards)
 
@@ -12,8 +22,8 @@ function createDeck(numberCards) {
 
     let cards = []
     for (let index = 0; index < numberCards / 2; index++) {
-        cards.push(index + 1);
-        cards.push(index + 1);       
+        cards.push(index);
+        cards.push(index);
     }
 
     cards = shuffle(cards);
@@ -22,19 +32,22 @@ function createDeck(numberCards) {
         let divBox = document.createElement('div');
         divBox.className = "box";
         container.appendChild(divBox);
+
     }
-    let rows = document.getElementsByClassName('box')
+    let rows = document.getElementsByClassName('box')    
     for (let i = 0; i < rows.length; i++) {
         for (let n = i * width; n < (i + 1) * width; n++) {
             let div = document.createElement('div');
-            div.innerHTML = cards[n];
+            let img = document.createElement('img');
             div.type = cards[n];
+            // div.innerHTML = cards[n]
             div.className = "mem";
+            img.className = "hiddenImage";
+            img.src = picture[cards[n]];
+            div.appendChild(img);
             rows.item(i).appendChild(div);
-            div.addEventListener('click', flipp(div))
         }
-    }
-    
+    }    
 }
 
 function shuffle(cards) {
@@ -50,14 +63,73 @@ function shuffle(cards) {
 }
 
 
-function flipp(div) {
-    count++;
-    console.log(count)
-    if (count < 3){        
-        console.log("Count:" + count)
-        div.style.background = "green";
-        if (count == 2){
-            console.log("count is 2")
+const divs = document.querySelectorAll('.mem')
+divs.forEach(div => {
+    div.addEventListener('click', () => {
+        count++;
+        firstClick = 1
+        if (firstClick == 1){
+            document.querySelector('#countdown').innerHTML = 'Time: ' + countTime + 's'
+            if(!timeractive){
+                timeractive = true
+                playInterval = setInterval(function myCallback(){
+                    countTime++
+                    document.querySelector('#countdown').innerHTML = 'Time: ' + countTime + 's'
+                }, 1000)
+            }
         }
-    }               
+        if (count < 3){     
+        div.classList.toggle('flipped')
+        div.querySelector('.hiddenImage').style.visibility = "visible"
+        if (count == 1){
+            list.push(div)
+        }
+        else if (count == 2){
+            list.push(div)
+            compareCards(list)
+            count = 0
+            list = []
+        }
+    }     
+    })    
+})
+
+function compareCards(list){
+    if (list[0].type == list[1].type){
+        found ++
+        if (numberCards == 16){
+            if (found == 8){
+                found = 0
+                if (highscore == 0){                    
+                    alert('Congratulations! You reached a new highscore.')
+                    highscore = countTime
+                    document.querySelector('#highscore').innerHTML = 'Highscore: ' + highscore + 's'
+                }
+                else if (highscore > countTime){
+                    alert('Congratulations! You reached a new highscore.')
+                    highscore = countTime
+                    document.querySelector('#highscore').innerHTML = 'Highscore: ' + highscore + 's'
+                }
+                clearInterval(playInterval)
+                timeractive = false
+                firstClick = 0
+                setTimeout(() => {
+                    document.querySelectorAll('.mem').forEach(div => {
+                        div.classList.toggle("flipped")
+                        div.querySelector('.hiddenImage').style.visibility = "hidden"
+                        countTime = 0
+                        document.querySelector('#countdown').innerHTML = 'Time: ' + countTime + 's'
+                    })
+                }, 2000)
+            }
+        }
+    }
+    else{
+        setTimeout(() => {
+            list[0].classList.toggle("flipped")
+            list[1].classList.toggle("flipped")
+            list[0].querySelector('.hiddenImage').style.visibility = "hidden"
+            list[1].querySelector('.hiddenImage').style.visibility = "hidden"
+        }, 600)     
+    }
 }
